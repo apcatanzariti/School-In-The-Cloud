1768d9444bcc3e5aa188c305b9e16a86475bfb96
Anthony Catanzariti <apcatanzariti@gmail.com>
2021-03-01 13:21:14 -0500

    login functionality set up to be plugged in once we get endpoints

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 function AdminLogin () {
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    });
+
+    const [error, setError] = useState('');
+
+    const history = useHistory();
+
+    function handleChange (e) {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    function handleSubmit (e) {
+        e.preventDefault();
+
+        // the 'else' portion of this will be replaced by the axios call
+        if (credentials.username === '' || credentials.password === '') {
+            setError('Username and Password must be filled out');
+        } else {
+            setError('');
+            console.log('Admin logged in!');
+        }
+
+        // axios
+        // .post('http://localhost:5000/api/login', credentials)
+        // .then(res => {
+        //     localStorage.setItem('token', JSON.stringify(res.data.payload));
+        //     history.push('/admin-dash');
+        // })
+        // .catch(err => {
+        //     setError(err.response.data.error);
+        // })
+    };
+
     return(
         <StyledAdminContainer>
             <h3>Admin Login Form</h3>
 
-            <form>
+            <form onSubmit={handleSubmit}>
 
                 <div>
                 <input 
                 name='username'
                 type='text'
-                placeholder='Username'/>
+                placeholder='Username'
+                value={credentials.username}
+                onChange={handleChange}/>
                 </div>
 
                 <div>
                 <input 
                 name='password'
                 type='password'
-                placeholder='Password'/>
+                placeholder='Password'
+                value={credentials.password}
+                onChange={handleChange}/>
                 </div>
 
                 <button>Admin Sign In</button>
+                <center><StyledError>{error}</StyledError></center>
 
             </form>
         </StyledAdminContainer>
@@ -43,6 +88,7 @@ const StyledAdminContainer = styled.div`
         padding: 1%;
         width: 60%;
         margin-bottom: 6%;
+        outline: none;
     }
 
     button {
@@ -52,10 +98,17 @@ const StyledAdminContainer = styled.div`
         padding: 2% 4% 2% 4%;
         transition: .3s;
         cursor: pointer;
+        outline: none;
     }
 
     button:hover {
         background-color: #0096DB;
         color: white;
     }
+`;
+
+const StyledError = styled.div`
+    color: red;
+    margin-top: 6%;
+    width: 70%;
 `;
---
+++ src/reducers/reducer.js
@@ -0,0 +1,12 @@
+import { bindActionCreators } from "redux";
+
+const initialState = {
+
+};
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    };
+};
