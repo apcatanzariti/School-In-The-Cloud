55cbc15210a3e3d0b680e492e03a27b283f675e0
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-03-01 13:15:40 -0500

    when click the cloud goes to homepage

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,78 +1,79 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-function NavBar () {
-    return(
-        <StyledNavContainer>
+function NavBar() {
+  return (
+    <StyledNavContainer>
+      <Link to="/">
+        <img
+          src="https://cloudevents.io/img/logos/integrations/triggermesh.png"
+          alt="cloud logo"
+        />
+      </Link>
 
-            <img src='https://cloudevents.io/img/logos/integrations/triggermesh.png' alt='cloud logo' />
+      <StyledLinksDiv>
+        <Link to="/">
+          <StyledLink>Home</StyledLink>
+        </Link>
 
-            <StyledLinksDiv>
+        <Link to="/admin-dash">
+          <StyledLink>Admin Dashboard</StyledLink>
+        </Link>
 
-                <Link to='/'>
-                    <StyledLink>Home</StyledLink>
-                </Link>
+        <Link to="/student-dash">
+          <StyledLink>Student Dashboard</StyledLink>
+        </Link>
 
-                <Link to='/admin-dash'>
-                    <StyledLink>Admin Dashboard</StyledLink>
-                </Link>
+        <Link to="/volunteer-dash">
+          <StyledLink>Volunteer Dashboard</StyledLink>
+        </Link>
 
-                <Link to='/student-dash'>
-                    <StyledLink>Student Dashboard</StyledLink>
-                </Link>
-
-                <Link to='/volunteer-dash'>
-                    <StyledLink>Volunteer Dashboard</StyledLink>
-                </Link>
-
-                <Link to='/'>
-                    <StyledLink>Logout</StyledLink>
-                </Link>
-
-            </StyledLinksDiv>
-
-        </StyledNavContainer>
-    );
-};
+        <Link to="/">
+          <StyledLink>Logout</StyledLink>
+        </Link>
+      </StyledLinksDiv>
+    </StyledNavContainer>
+  );
+}
 
 export default NavBar;
 
 const StyledNavContainer = styled.div`
-    // border: 1px solid red;
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
-    border-bottom: solid 1px #0096DB;
+  // border: 1px solid red;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  border-bottom: solid 1px #0096db;
 
-    img {
-        // border: solid 1px blue;
-        height: 130px;
-    }
+  img {
+    // border: solid 1px blue;
+    height: 130px;
+  }
 `;
 
 const StyledLinksDiv = styled.div`
-    // border: solid 1px yellow;
-    display: flex;
-    justify-content: space-evenly;
-    width: 80%;
-    align-items: center;
-    font-size: 1.2em;
-    margin-right: 4.5%;
+  // border: solid 1px yellow;
+  display: flex;
+  justify-content: space-evenly;
+  width: 80%;
+  align-items: center;
+  font-size: 1.2em;
+  margin-right: 4.5%;
 
-    a {
-        text-decoration: none;
-        color: #0096DB;
-        transition: .3s;
-    }
+  a {
+    text-decoration: none;
+    color: #0096db;
+    transition: 0.3s;
+  }
 
-    a:hover {
-        color: white;
-        background-color: #0096DB;
-    }
+  a:hover {
+    color: white;
+    background-color: #0096db;
+  }
 `;
 
 const StyledLink = styled.div`
-    border-radius: 5px;
-    padding: 7px 12px 7px 12px;
+  border-radius: 5px;
+  padding: 7px 12px 7px 12px;
 `;
