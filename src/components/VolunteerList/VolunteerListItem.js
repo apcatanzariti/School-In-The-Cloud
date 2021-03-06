678bd0df63eb4a96e9737551bc5f5acadbb9f808
Jonathan Fox <FoxJonathanP@gmail.com>
2021-03-05 17:49:38 -0800

    Got volunteers working with country and availability in student dash :D

--- src/components/VolunteerList/VolunteerListItem.js
+++ src/components/VolunteerList/VolunteerListItem.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+
+import { availabilityDetails } from '../../utils/volunteerApi';
 
 
 
 function VolunteerListItem(props) {
 
-    const { volunteer } = props;
+  const { volunteer } = props;
+  const [ availability, setAvailability ] = useState('');
 
-    return (
-        <StyledVolunteerListItemDiv>
-            <div className='name-row'>
-                <p className='name'>{volunteer.username}</p>
-                {volunteer.country &&
-                  <p className='country'>{volunteer.country}</p>
-                }
-            </div>
-            {/* <p className='timeSlots'>Available: {timeSlots}</p> */}
-        </StyledVolunteerListItemDiv>
-    );
+  useEffect(() => {
+    setAvailability( availabilityDetails[volunteer.availability]?.name );
+  }, [ volunteer ]);
+
+  return (
+    <StyledVolunteerListItemDiv>
+      <div className='name-row'>
+        <p className='name'>{volunteer.username}</p>
+        {volunteer.country &&
+          <p className='country'>{volunteer.country}</p>
+        }
+      </div>
+      <p className='availability'>Available: {availability}</p>
+    </StyledVolunteerListItemDiv>
+  );
 }
 
 export default VolunteerListItem;
@@ -26,30 +33,31 @@ export default VolunteerListItem;
 
 const StyledVolunteerListItemDiv = styled.div`
 
-    padding: 10px 15px;
-    border-bottom: 1px solid #d5d5d5;
+  padding: 10px 15px;
+  border-bottom: 1px solid #d5d5d5;
 
-    &:last-child {
-        border-bottom: none;
-    }
+  &:last-child {
+    border-bottom: none;
+  }
 
-    p {
-        margin: 5px 0;
-    }
+  p {
+    margin: 5px 0;
+  }
 
-    .name-row {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-    }
+  .name-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-    .name {
-        font-size: 1.2em;
-        font-weight: bold;
-    }
+  .name {
+    font-size: 1.2em;
+    font-weight: bold;
+  }
 
-    .country {
-        color: gray;
-    }
+  .country {
+    color: gray;
+    text-transform: capitalize;
+  }
 `;
